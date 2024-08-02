40077865787224fad24520b31750c3e2bc481bbe
Darkrender <jimmiegisclair@gmail.com>
2024-08-02 13:18:26 -0500

    Update default colorscheme, fix which-key deprecations, and use a different statusline plugin

--- lua/darkrender/lazy/colors.lua
+++ lua/darkrender/lazy/colors.lua
@@ -1,8 +1,12 @@
 function ColorMyPencils(color)
-  color = color or 'tokyonight-night'
+  color = color or 'catppuccin-mocha'
   vim.cmd.colorscheme(color)
   vim.cmd.hi = 'Comment gui=none'
 end
+
+vim.api.nvim_create_user_command('ColorMyPencils', function(opts)
+  ColorMyPencils(opts.fargs[1])
+end, { nargs = '?' })
 
 return {
   {
@@ -13,5 +17,11 @@ return {
     config = function()
       ColorMyPencils()
     end,
+  },
+  {
+    'Mofiqul/vscode.nvim',
+  },
+  {
+    'catppuccin/nvim',
   },
 }
